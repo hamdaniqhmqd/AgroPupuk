cfbed46f6fb36d441c8c479b54ac971a5d2cacd4
Mazu <masdwenda@gmail.com>
2024-03-28 23:46:33 +0700

    Tampilan dashboard

---
+++ resources/views/dashboard/halaman.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!-- Masukkan link CSS Anda di sini -->
+    <link rel="stylesheet" href="{{ ('css/app.css') }}">
+    
+</head>
+
+<body>
+    
+@include('utility.sidebar')
+    
+    <div class="main-content-container">
+        <div class="main-content">
+            <h1>Dashboard Content Halaman</h1>
+            
+            <button onclick="window.location='{{ route('formprod') }}'" class="btn">[~] Edit </button>
+            
+            <!-- Isi konten utama -->
+            <p>Selamat datang di Halaman Anda!</p>
+            <div class="main">
+                <p>
+                    test
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Masukkan link JavaScript Anda di sini -->
+   
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,25 @@ use App\Http\Controllers\ControllerLamanAdminBerita;
 use App\Http\Controllers\ControllerLamanBerita;
 use App\Http\Controllers\ControllerLamanUtama;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FormController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::resource('/home', ControllerLamanUtama::class);
 
 Route::get('/berita', [ControllerLamanBerita::class,'index'])->name('berita.index');
 
 Route::resource('/admin_berita', ControllerLamanAdminBerita::class);
+
+//DASHBOARD
+Route::get('/profile', [DashboardController::class, 'showProfile'])->name('profil');
+Route::get('/products', [DashboardController::class, 'showProducts'])->name('produk');
+Route::get('/posts', [DashboardController::class, 'showPosts'])->name('postingan');
+Route::get('/halaman}', [DashboardController::class, 'showPage'])->name('halaman');
+//FORM
+Route::get('utility/form', [FormController::class, 'showForm'])->name('formprod');
+
