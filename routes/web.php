faa453c5d50310bae3a82f5e9accc4e422ecb392
Haqqi <samodrahaqqi30@gmail.com>
2024-03-28 22:56:38 +0700

    list produk cart, sort done, kurang navbar

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('list-produk');
+});
+
+Route::get('/petronitrat.blade.php', function () {
+    return view('petronitrat');
+});
+
+Route::get('/phonska.blade.php', function () {
+    return view('phonska');
+});
+
+Route::get('/urea.blade.php', function () {
+    return view('urea');
+});
+
+Route::get('/za.blade.php', function () {
+    return view('za');
 });
